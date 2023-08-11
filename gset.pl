2d868ae3e74321143602ed6db6b8db18d45e40b7
Borek Lupomesky <borek@lupomesky.cz>
2023-08-11 11:19:44 +0200

    Improve some comments

--- gset.pl
+++ gset.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/env perl
 
+# script for generating index of galleries; this scans either current or
+# user-supplied directory for subdirectories, checking if they contain
+# 'info.json' file; the list of directories with some additional info is then
+# saved into 'gset.json', which can then be used by custom frontend code
+
 use warnings;
 use strict;
 use feature qw(say);
@@ -17,35 +22,35 @@ my $total_videos = 0;
 my $p = path($ARGV[0] // cwd);
 say 'Base path is ', $p;
 
-# iterate over files
+# iterate over directories
 my $iter = $p->iterator;
 while (my $r = $iter->()) {
   my %data;
-  
-  # basic processing
+
+  # skip anything that isn't a directory with info.json in it
   next unless $r->is_dir;
   my $info_file = $r->child('info.json');
   next unless $info_file->is_file;
   my $dir = $r->basename;
   push(@dirs, $dir);
 
-  # read info file  
+  # read and parse the file
   my $info = decode_json($info_file->slurp_raw);
   $data{title} = $info->{title} // $dir;
   $data{date} = $info->{date} // undef;
-  
+
   # get number of images/videos
   $data{images} = $r->children(qr/^(?!thumb).*\.jpg\z/i);
   $data{videos} = $r->children(qr/\.mp4\z/i);
   $total_images += $data{images} // 0;
   $total_videos += $data{videos} // 0;
-  
+
   # thumbnails
   $data{thumb}{src} = "$dir/thumb.2x.jpg";
   $data{thumb}{srcset} = "$dir/thumb.1x.jpg 1x, $dir/thumb.2x.jpg 2x";
 
   # save data
-  $dirinfo{$dir} = \%data;  
+  $dirinfo{$dir} = \%data;
 }
 
 # print report
@@ -54,7 +59,7 @@ printf("Found %d images and %d videos\n", $total_images, $total_videos);
 
 # sort the directories
 @dirs = sort { $b cmp $a } @dirs;
- 
+
 # save gset
 $p->child('gset.json')->spew_raw(
   encode_json({
@@ -67,7 +72,7 @@ say 'Saved global gset';
 # iterate over every directory and set chaining fields
 my $prev;
 foreach my $dir (@dirs) {
-  # chaining links  
+  # chaining links
   if(defined $prev) {
     $dirinfo{$prev}{prev} = $dir;
     $dirinfo{$dir}{next} = $prev;
